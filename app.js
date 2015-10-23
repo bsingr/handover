b93906959b98c52e3445fc52ae8ecf53ec94798d
Jens Bissinger <mail@jens-bissinger.de>
2015-10-23 19:00:14 +0200

    Expose shared files via http and not via node-discover

--- app.js
+++ app.js
@@ -6,10 +6,13 @@ var globalShortcut = require('global-shortcut');
 var Discover = require('node-discover');
 var NativeImage = require('native-image');
 var fs = require('fs');
-var yenc = require('yenc');
 var dialog = require('dialog');
 var path = require('path');
 var mime = require('mime');
+var request = require('superagent');
+
+var koa = require('koa');
+var http = require('http');
 
 var icon = NativeImage.createFromPath(__dirname + '/resources/icon.png');
 var dropIcons = {
@@ -21,15 +24,21 @@ var dropIcons = {
 var d = Discover();
 
 var appIcon = null;
-var lastReceive = null;
+var httpPort = null;
 
-function receiveData(data) {
+var lastReceive = null;
+var lastSend = {};
+
+function receiveNotice(data, obj) {
   appIcon.setImage(dropIcons[resolveDropIconName(data)]);
-  lastReceive = data;
+  lastReceive = {
+    data: data,
+    obj: obj
+  };
 }
 
 function resolveDropIconName(data) {
-  if (data.type === 'text') {
+  if (data.mime.match(/text/)) {
     return 'text';
   } else if (data.mime.match(/image/)) {
     return 'image';
@@ -38,36 +47,57 @@ function resolveDropIconName(data) {
   }
 }
 
-function send() {
-  var success = d.send("clipboard", { payload : clipboard.readText(), type: 'text' });
+function sendData(data) {
+  lastSend = data;
+  var success = d.send("clipboard", lastSend);
+}
+
+function sendText() {
+  sendData({
+    payload: clipboard.readText(),
+    mime: 'text/plain',
+    httpPort: httpPort
+  });
 }
 
 function sendFiles(paths) {
-  var path = paths[0];
-  var mimeType = mime.lookup(path);
-  var encoded = yenc.encodeBytes(fs.readFileSync(path));
-  var data = { payload: encoded, type: 'file', encoding: 'yenc', mime: mimeType };
-  var success = d.send("clipboard", data);
+  var firstPath = paths[0];
+  var mimeType = mime.lookup(firstPath);
+  sendData({
+    mime: mimeType,
+    path: firstPath,
+    httpPort: httpPort
+  });
 }
 
-function receive(data) {
-  appIcon.setImage(icon);
-  if (data.type === 'text') {
-    clipboard.writeText(data);
-  } else {
-    dialog.showSaveDialog({
-      title: 'Choose location to store the .' + mime.extension(data.mime) + ' file'
-    }, function(path){
-      if (path) {
-        var decoded = yenc.decodeBytes(data.payload);
-        var buffer = new Buffer(decoded);
-        fs.writeFileSync(path, buffer);
-      }
-    });
-  }
+function receive(notice) {
+  d.eachNode(function(node){
+    if (node.id === notice.obj.iid) {
+      request('http://'+node.address+':'+notice.data.httpPort)
+        .send()
+        .end(function(err, res){
+          if (err) {
+            console.log('Err', err);
+            return;
+          }
+          appIcon.setImage(icon);
+          if (notice.data.mime.match(/text\/plain/)) {
+            clipboard.writeText(res.text);
+          } else {
+            dialog.showSaveDialog({
+              title: 'Choose location to store the .' + mime.extension(notice.data.mime) + ' file'
+            }, function(destinationPath){
+              if (destinationPath) {
+                fs.writeFileSync(destinationPath, res.body);
+              }
+            });
+          }
+        });
+    }
+  });
 }
 
-d.join("clipboard", receiveData);
+d.join("clipboard", receiveNotice);
 
 app.dock.hide();
 app.on('ready', function(){
@@ -76,7 +106,7 @@ app.on('ready', function(){
   appIcon.on('drop-files', function(e, files){
     sendFiles(files);
   });
-  globalShortcut.register('CmdOrCtrl+shift+c', send);
+  globalShortcut.register('CmdOrCtrl+shift+c', sendText);
   globalShortcut.register('CmdOrCtrl+shift+v', function(){
     if (lastReceive) {
       receive(lastReceive);
@@ -86,4 +116,27 @@ app.on('ready', function(){
 
 app.on('will-quit', function() {
   globalShortcut.unregisterAll();
+});
+
+var webApp = koa();
+webApp.use(function *(){
+  if (!lastSend) {
+    this.status = 404;
+    return;
+  }
+  this.type = lastSend.mime;
+  if (lastSend.payload) {
+    this.body = lastSend.payload;
+  } else if (lastSend.path) {
+    this.body = fs.createReadStream(lastSend.path);
+  }
+  if (lastSend.path && !lastSend.mime.match(/(text|json|xml|gif|png|jpg)/)) {
+    this.attachment(path.basename(lastSend.path));
+  }
+});
+var httpServer = http.createServer(webApp.callback());
+httpServer.listen(0); // random
+httpServer.on('listening', function() {
+  httpPort = httpServer.address().port;
+  console.log('listening http://127.0.0.1:'+httpPort);
 });
