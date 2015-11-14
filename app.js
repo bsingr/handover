c0ce9426a4cb43d8927caa5eec478d15eee016a0
Jens Bissinger <mail@jens-bissinger.de>
2015-11-14 20:39:20 +0100

    Revert "Bootstrap mdns"
    This reverts commit faa847120a84c83341f27619c41f79a06c5c6dc0.

--- app.js
+++ app.js
@@ -71,53 +71,6 @@ d.join("clipboard", (data, obj) => {
   })
 })
 
-function buildClientUrl(host, port) {
-  return 'http://'+host+':'+port
-}
-
-var mdns = require('mdns-js');
-//if you have another mdns daemon running, like avahi or bonjour, uncomment following line
-//mdns.excludeInterface('0.0.0.0');
-
-var browser = mdns.createBrowser(mdns.tcp('handover'));
-browser.on('ready', function () {
-  browser.discover();
-});
-
-browser.on('update', function (data) {
-  if (!httpPort || !data.port) {
-    return
-  }
-  if (data.port === httpPort) {
-    var myAddresses = buildAddresses()
-    for (let idx in myAddresses) {
-      let myAddress = myAddresses[idx]
-      if (data.addresses.indexOf(myAddress) > -1) {
-        return
-      }
-    }
-  }
-  var clientUrl = buildClientUrl(data.addresses[0], data.port);
-  console.log(clientUrl, 'detected', data)
-});
-
-import os from 'os'
-
-function buildAddresses() {
-  var addresses = []
-  var nics = os.networkInterfaces()
-  for (let key of Object.keys(nics)) {
-    let nic = nics[key]
-    for (let idx in nic) {
-      let nicAddress = nic[idx]
-      if (!nicAddress.internal) {
-        addresses.push(nicAddress.address)
-      }
-    }
-  }
-  return addresses
-}
-
 app.dock ? app.dock.hide() : false // disable dock icon on OS X
 
 app.on('ready', () => {
@@ -143,13 +96,4 @@ httpServer.listen(0) // random
 httpServer.on('listening', () =>  {
   httpPort = httpServer.address().port
   console.log('listening http://127.0.0.1:'+httpPort)
-  var addresses = buildAddresses()
-  for (let idx in addresses) {
-    let address = addresses[idx]
-    var service = mdns.createAdvertisement(mdns.tcp('handover'), httpPort, {
-      name: 'handover',
-      host: address
-    });
-    service.start();
-  }
 })
