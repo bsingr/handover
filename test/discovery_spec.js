6232f3e255828df334044486f91723e69945e418
Jens Bissinger <mail@jens-bissinger.de>
2016-01-10 23:08:57 +0100

    Add test

---
+++ test/discovery_spec.js
@@ -0,0 +1,16 @@
+'use babel'
+
+import assert from 'assert'
+import Discovery from '../src/discovery'
+
+describe('Discover', () => {
+  let subject
+
+  beforeEach(() => subject = new Discovery())
+
+  describe('#findNodeById', () => {
+    it('returns undefined', () => {
+      assert.equal(undefined, subject.findNodeById('foo'))
+    })
+  })
+})
