302c5b887cc581f9ea03b9f7efe3a542de241b00
Andreas Bissinger <mail@bissinger-andreas.de>
2018-10-03 09:28:43 +0200

    handle file uploads and improve context menu

--- src/util.js
+++ src/util.js
@@ -18,3 +18,16 @@ export function handlePaste(client, consumer) {
   }
 }
 
+export function shareFile(publisher) {
+  dialog.showOpenDialog(
+    {
+      buttonLabel: 'Share'
+    },
+    filePaths => {
+      if (Array.isArray(filePaths) && filePaths.length > 0) {
+        publisher.push(new FilePayload(filePaths[0]))
+      }
+    }
+  )
+}
+
